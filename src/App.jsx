e669fedf6d0ef919245dffeeb8ad869d8fbf77c5
Nicko Balboa <76141006+NBalboa@users.noreply.github.com>
2024-03-09 20:13:40 +0800

    Register Page Added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import Register from "./pages/Register";
 function App() {
     return (
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={<Home />}></Route>
+                <Route path="/register" element={<Register />}></Route>
             </Routes>
         </BrowserRouter>
     );
